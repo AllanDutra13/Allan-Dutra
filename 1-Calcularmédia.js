22e37c9fdc5730448cf760bdf710d3e288ccf211
Allan Dutra <allandutra1320@gmail.com>
2024-10-07 18:51:40 -0300

    1-Calcularmédia.js

--- 1-Calcularmédia.js
+++ 1-Calcularmédia.js
@@ -1,6 +1,4 @@
-    <title>mediamediaprova</title>
     
-        <script>
             function mediaProva(a, b, c) {
                 let media = (a + b + c) / 3;
                 var situacao;
@@ -16,6 +14,5 @@
             let nota2 = parseFloat(prompt("Digite o segunda nota:"));
             let nota3 = parseFloat(prompt("Digite o terceira nota:"));
             mediaProva(nota1, nota2, nota3);
-        </script>
 
     
